affdab4296332c6d88e07e0292627a164c2398e0
jeydpeaschocolatebar <jade.palco@mllrdev.com>
2023-04-27 11:32:01 +0800

    gtk: layouts

---
+++ src/layouts/products.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Tanglad Merch</NuxtLink>
+            </nav>
+        </header>
+
+        <!-- output the page content -->
+        <div>
+            <slot />
+        </div>
+
+        <footer>
+            <ul>
+                <li>
+                    <NuxtLink to="/">Home</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/about">About</NuxtLink>
+                </li>
+                <li>
+                    <NuxtLink to="/products">Products</NuxtLink>
+                </li>
+            </ul>
+        </footer>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'products'
+})
+</script>
+
+<style scoped>
+.router-link-exact-active {
+    color: #12b488
+}
+</style>
