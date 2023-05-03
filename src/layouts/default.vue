861b46c2bf02105d60f9ae573038eb5e66399c9c
jeydpeaschocolatebar <jade.palco@mllrdev.com>
2023-05-03 15:59:44 +0800

    add: tailwind css

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">House of Tanglads</NuxtLink>
-                <ul>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <NuxtLink to="/" class="font-bold">House of Tanglads</NuxtLink>
+                <ul class="flex gap-4">
                     <li>
                         <NuxtLink to="/">Home</NuxtLink>
                     </li>
@@ -11,14 +11,14 @@
                         <NuxtLink to="/about">About</NuxtLink>
                     </li>
                     <li>
-                        <NuxtLink to="/products">Products</NuxtLink>
+                        <NuxtLink to="/products" class="btn">Products</NuxtLink>
                     </li>
                 </ul>
             </nav>
         </header>
 
         <!-- output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
     </div>
--- src/layouts/products.vue
+++ src/layouts/products.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
-        <header>
-            <nav>
-                <NuxtLink to="/">Tanglad Merch</NuxtLink>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-aut p-4">
+                <NuxtLink to="/" class="font-bold ">Tanglad Merch</NuxtLink>
             </nav>
         </header>
 
         <!-- output the page content -->
-        <div>
+        <div class="container mx-auto p-4">
             <slot />
         </div>
 
-        <footer>
-            <ul>
+        <footer class="container mx-auto p-4 flex justify-between border-t-2">
+            <ul class="flex gap-4">
                 <li>
                     <NuxtLink to="/">Home</NuxtLink>
                 </li>
